b804a542349ec9a2a76dd4a15c3cf3fb35559315
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-05 11:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function stringChop(str, size) {
   // your code here
-	if(!str.length || str.length > 10^6) return;
-	if(size >)
+	// if(!str.length || str.length > 10^6) return;
+	// if(size >)
     let tempStrArr = str.trim().split('');
 	const chunkArr = [];
 	while(tempStrArr.length) chunkArr.push(tempStrArr.splice(0,size).join(''));
 	return chunkArr;
 }
 
-// Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
-// alert(stringChop(str, size));
+Do not change the code below
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));
