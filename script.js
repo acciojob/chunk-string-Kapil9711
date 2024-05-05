b61803e3d34d0b704ae87251f34ed034351cb9f4
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-05 11:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function stringChop(str, size) {
-  // your code here
-	// if(!str.length || str.length > 10^6) return;
-	// if(size >)
     let tempStrArr = str.trim().split('');
 	const chunkArr = [];
 	while(tempStrArr.length) chunkArr.push(tempStrArr.splice(0,size).join(''));
